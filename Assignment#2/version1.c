8192b06c16d7879e287c87350d7d9dbeff250fd7
Anas <anas.munir.92@gmail.com>
2015-04-09 21:01:53 +0500

    Update version1.c

--- Assignment#2/version1.c
+++ Assignment#2/version1.c
@@ -5,66 +5,76 @@
 #include <fcntl.h>
 #define CREATE_FLAGS (O_WRONLY | O_CREAT | O_APPEND)
 #define CREATE_MODE (S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH)
-main()
+
+/* tail -5 /etc/passwd | grep e | sort */
+char *cmd2[] = {"grep","e", NULL};
+char *cmd1[] = {"tail","-5", "/etc/passwd", NULL};
+
+int main(void)
 {
-    pid_t pid1, pid2;
-    int fd1[2];
-    int fd2[2];
-    char *arg[4];
-    char *arg2[3];
-    char *arg3[3];  
+    int pid_1, pid_2;
+    int fd1[2], fd2[2]; 
     
-   
-    /* tail -5 /etc/passwd | grep e | sort */
-    /* -----process 1------|process2|prcoess3*/
-    int newfd = open("YoYo.txt", CREATE_FLAGS, CREATE_MODE);
+    int newfd;
+
+    newfd = open("result.txt", CREATE_FLAGS, CREATE_MODE);
         dup2(newfd, STDOUT_FILENO);
         close(newfd);
 
-    pipe(fd1); 
-    pid1 = fork();
+    if((pipe(fd1) == -1) || ((pid_1 = fork()) == -1)) {
+        perror("Failed to setup 1st pipeline");
+        return 1;
+    }
 
-    if (pid1 == 0) { /* Child process 2 */
-        
-        pipe(fd2);
-      
-        pid2=fork();
-    
-        if (pid2 == 0) { /* grandchild process 1*/
-            
-            close (fd1[1]), close (fd1[0]);
-            
-            
-            dup2 (fd2[1],1);
-            close (fd2[1]), close (fd2[0]);
-            arg[0] = "tail";
-            arg[1] = "-5";
-            arg[2] = "/etc/passwd";
-            arg[3] = NULL;
-            execvp("tail", arg);
-            perror("execl second child");
+    if (pid_1 == 0) { 
 
-            wait(NULL) != pid2;
-        }else { /* executing child process 2 */
-        
-            dup2(fd2[0],0);
-            close (fd2[0]), close (fd2[1]);
-            
-            dup2(fd1[1],1);
-            close (fd1[1]), close (fd1[0]);
-            arg2[0] = "grep";
-            arg2[1] = "e";
-            arg2[2] = NULL;
-            execvp("grep", arg2);
-            perror("execl first child");
-            wait(NULL) != pid1;
+        if((pipe(fd2) == -1) || ((pid_2 = fork()) == -1)){ 
+            perror("Failed to setup 2nd pipeline");
+            return 1;
         }
-    }
-    else { /* executing parent process 3 */
-        
-        dup2(fd1[0],0);
-        close (fd1[0]), close (fd1[1]);
-        execl("/usr/bin/sort", "sort", "-n", NULL);
-        perror("execl father");    
-    }
-} 
+        if (pid_2 == 0) {
+
+            if ((close (fd1[1]) == -1) ||  (close (fd1[0]) == -1)){
+            perror("fail to close the first pipe");
+            return 1;
+            }
+
+            if(dup2(fd2[1],1) == -1)
+                perror("Failed in pid_2");
+            else if ((close(fd2[1]) == -1) || (close(fd2[0]) == -1))
+                perror("failed");
+            else {
+                execvp(cmd1[0], cmd1);
+                perror("execvp second child");
+            }
+            return 1;
+        } //end of second child
+
+            if(dup2(fd2[0],0) == -1)
+                perror("Failed for std in");
+            if ((close(fd2[0]) == -1) || (close(fd2[1]) == -1))
+                perror("failed");
+           
+
+            if(dup2(fd1[1], 1) == -1)
+                perror("failed for std_out");
+            else if ((close(fd1[1]) == -1) ||  (close(fd1[0]) == -1))
+                perror("failed");
+            else {
+                execvp(cmd2[0], cmd2);
+                perror(cmd2[0]);
+            }
+            return 1;
+    } //end of first child
+    
+        if(dup2(fd1[0],0) == -1)
+            perror("failed");
+        else if ((close (fd1[0]) == -1) ||  (close (fd1[1]) == -1))
+            perror(" failed");
+        else {
+           
+            execl("/usr/bin/sort", "sort", NULL);
+            perror("execl father"); 
+        }
+        return 1; 
+    } 
